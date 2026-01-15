0588c5b4de0ff71af10c52c976f47bb3854f7d43
hwankeee <kee@voyagergames.gg>
2026-01-15 19:11:23 +0900

    add new input system (UMI)

---
+++ Assets/Scripts/NewInputFieldManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UMI;
+
+public class NewInputFieldManager : MonoBehaviour
+{
+    void Awake()
+    {
+        MobileInput.Init();
+        MobileInput.OnKeyboardAction += OnKeyboardAction;
+        MobileInput.OnOrientationChange += OnOrientationChange;
+    }
+
+    void OnOrientationChange(HardwareOrientation orientation)
+    {
+        // raise when the screen orientation is changed
+        Debug.Log(orientation);
+    }
+
+    void OnKeyboardAction(bool isShow, int height)
+    {
+        // raise when the keyboard is displayed or hidden, and when the keyboard height is changed
+        Debug.Log($"isShow: {isShow}, height: {height}");
+    }
+}
