91df3d1ceebc25c2f8d8bbc37e200fcafd0397f4
hwankeee <kee@voyagergames.gg>
2026-01-15 19:50:51 +0900

    commit done

--- Assets/Scripts/NewInputFieldManager.cs
+++ Assets/Scripts/NewInputFieldManager.cs
@@ -3,20 +3,20 @@ using UMI;
 
 public class NewInputFieldManager : MonoBehaviour
 {
-    void Awake()
+    private void Awake()
     {
         MobileInput.Init();
         MobileInput.OnKeyboardAction += OnKeyboardAction;
         MobileInput.OnOrientationChange += OnOrientationChange;
     }
 
-    void OnOrientationChange(HardwareOrientation orientation)
+    private void OnOrientationChange(HardwareOrientation orientation)
     {
         // raise when the screen orientation is changed
         Debug.Log(orientation);
     }
 
-    void OnKeyboardAction(bool isShow, int height)
+    private void OnKeyboardAction(bool isShow, int height)
     {
         // raise when the keyboard is displayed or hidden, and when the keyboard height is changed
         Debug.Log($"isShow: {isShow}, height: {height}");
